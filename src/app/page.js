a51061c4c1bc50e799ae23c5a1f74c797a3d0f73
Akashp-wan <akash.p@wanbuffer.in>
2025-09-25 14:33:53 +0530

    Updated code in demo-nextjs-app

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <main>
       <h1>Hello, this is a demo Next.js app 🚀</h1>
+      <button>gdwjhe</button>
+      <>
+      you can modify anything here
+      </>
     </main>
   );
 }
+
