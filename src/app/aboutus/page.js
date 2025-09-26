1635fab9301217fba472ea12c682c1efdd39145f
Akashp-wan <akash.p@wanbuffer.in>
2025-09-26 10:46:39 +0530

    adding-readmore-span

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client"; 
+import React, { useState } from 'react'
 
 function About() {
+  const [showMore, setShowMore] = useState(false);
+
   return (
-    <div>About</div>
-  )
+    <div>
+      <h2>About</h2>
+      <p>
+        This is a short description about the project.
+        {showMore && (
+          <span>
+            {" "}
+            Here’s some extra detailed information that appears when you click
+            Read More. You can write anything you want here.
+          </span>
+        )}
+      </p>
+
+      <span
+        onClick={() => setShowMore(!showMore)}
+        style={{
+          color: "blue",
+          cursor: "pointer",
+          marginLeft: "8px",
+          textDecoration: "underline",
+        }}
+      >
+        {showMore ? "Read Less" : "Read More"}
+      </span>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/app/readmore/page.js
+++ src/app/readmore/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Read = () => {
   return (
+    <>
     <div> Read</div>
+    <span>ghfjgf jhhgfgf dga ghfd dscsd</span>
+    </>
   )
 }
 
